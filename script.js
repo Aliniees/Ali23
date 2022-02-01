7f59e3c591d2a94553a8527d1f48504c8e4a7b45
Aliniees <kulygina01@mail.ru>
2022-02-01 16:14:20 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(window).on("load", start);
+
+function start(){
+
+    $("body").css("background-color", "hotpink");
+    $("ul.nav a:first").click();
+}
+
+$(document).on("click", "ul.nav a", nav);
+
+function nav(e){
+    e.preventDefault();
+    //alert("lol");
+    var url = $(this).attr("href");
+    $("#content").load(url);
+    $("ul.nav a").removeClass("active");
+    $(this).addClass("active");
+}
+
+
